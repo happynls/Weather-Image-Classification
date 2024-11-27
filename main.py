83c2fc21195adb1d45d054a1ca5198a6b2948fac
happynls <happy.patel@nexuslinkservices.in>
2024-11-27 17:23:01 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from Weather_Image_Classification import logger
 from Weather_Image_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Weather_Image_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 STAGE_NAME_INGESTION = 'Data Ingestion Stage'
-
-
 try:
    logger.info(f">>>>>> stage {STAGE_NAME_INGESTION} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
@@ -13,3 +13,16 @@ except Exception as e:
         logger.exception(e)
         raise e
     
+    
+
+
+STAGE_NAME_PREPARE_BASE_MODEL = 'Prepare Base Model'
+try:
+    logger.info(f'\n\n{"**"*50}\nStarted {STAGE_NAME_PREPARE_BASE_MODEL}\n{"**"*50}\n')
+    prepare_model = PrepareBaseModelTrainingPipeline()
+    prepare_model.main()
+    logger.info(f'\n\n{"**"*50}\nCompleted {STAGE_NAME_PREPARE_BASE_MODEL}\n{"**"*50}\n\n')
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/Weather_Image_Classification/config/configuration.py
+++ src/Weather_Image_Classification/config/configuration.py
@@ -1,6 +1,7 @@
 from Weather_Image_Classification.constants import *
 from Weather_Image_Classification.utils.common import read_yaml , create_directories
-from Weather_Image_Classification.entity.config_entity import DataIngestionConfig
+from Weather_Image_Classification.entity.config_entity import (DataIngestionConfig ,
+                                                               PrepareBaseModelConfig)
 
 
 class ConfigurationManager:
@@ -28,3 +29,23 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+    
+    
+    
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_classes=self.params.CLASSES,
+            params_include_top=self.params.INCLUDED_TOP,
+            params_weights=self.params.WEIGHTS
+        )
+
+        return prepare_base_model_config
