90b49c236bfba6380f6b315dd80229998a06c485
happynls <happy.patel@nexuslinkservices.in>
2024-11-28 15:04:08 +0530

    prepare callbacks added

--- src/Weather_Image_Classification/config/configuration.py
+++ src/Weather_Image_Classification/config/configuration.py
@@ -1,8 +1,9 @@
 from Weather_Image_Classification.constants import *
 from Weather_Image_Classification.utils.common import read_yaml , create_directories
 from Weather_Image_Classification.entity.config_entity import (DataIngestionConfig ,
-                                                               PrepareBaseModelConfig)
-
+                                                               PrepareBaseModelConfig , 
+                                                               PrepareCallbacksConfig)
+import os
 
 class ConfigurationManager:
     def __init__(self,
@@ -49,3 +50,19 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+
+
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([Path(model_ckpt_dir),
+                            Path(config.tensorboard_root_log_dir)
+                            ])
+        
+        preapre_callbacks_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath),
+        )
+        
+        return preapre_callbacks_config
