c7e03124c7219a2b1af84fffadd0a5c76b1e2384
happynls <happy.patel@nexuslinkservices.in>
2024-11-29 16:57:15 +0530

    user app & prediction added

---
+++ src/Weather_Image_Classification/pipeline/predict.py
@@ -0,0 +1,31 @@
+import os
+from tensorflow.keras.models import load_model # type: ignore
+from tensorflow.keras.preprocessing import image # type: ignore
+import numpy as np
+
+
+class PredictionPipeline:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def predict(self):
+        # Load model
+        model = load_model(os.path.join("artifacts", "training", "model.keras"))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size=(224, 224))
+        test_image = image.img_to_array(test_image)
+        test_image = test_image / 255.0  # Normalize the image
+
+        # Expand dimensions to match the input shape expected by the model
+        test_image = np.expand_dims(test_image, axis=0)
+
+        # Make the prediction
+        predictions = model.predict(test_image)
+        print('----------->', predictions, '<-----------')
+        predicted_class_index = np.argmax(predictions)
+        class_labels = sorted(os.listdir('artifacts/data_ingestion/Weather_image_classification'))  # Replace with the actual path
+
+        prediction = class_labels[predicted_class_index]
+
+        return np.max(predictions), prediction
